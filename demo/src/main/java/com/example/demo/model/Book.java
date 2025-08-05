7cbfaa87e9c3f6bda3625d139835b36d1ce671c3
pari <parivashcheraghi40@gmail.com>
2025-08-05 12:01:55 +0330

    Book Entity added

---
+++ demo/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Book {
+
+
+    public Book() {
+    }
+
+    public Book(String title,String author,Integer publicationYear){
+        this.title=title;
+        this.author=author;
+        this.publicationYear=publicationYear;
+    }
+
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Title cannot be empty")
+    private String title;
+
+    @NotBlank(message = "Author cannot be empty")
+    private String author;
+
+    @Min(value = 1000 , message = "Year must be a valid number")
+    private Integer publicationYear;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + publicationYear +
+                '}';
+    }
+}
