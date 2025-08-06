fe11dabcfb4938a71fbd12e2178b3d7084505eda
pari <parivashcheraghi40@gmail.com>
2025-08-06 12:13:59 +0330

    Controller for user added

---
+++ demo/src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.security.JwtUtil;
+import com.example.demo.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private UserService userService;
+    private  JwtUtil jwtUtil;
+
+    public AuthController(UserService userService, JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        try {
+            User savedUser = userService.registerUser(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        return userService.findByUsername(user.getUsername())
+                .filter(u -> userService.passwordEncoder().matches(user.getPassword(), u.getPassword()))
+                .map(u -> ResponseEntity.ok(jwtUtil.generateToken(u.getUsername())))
+                .orElseGet(() -> ResponseEntity.status(401).body("Invalid username or password"));
+    }
+}
