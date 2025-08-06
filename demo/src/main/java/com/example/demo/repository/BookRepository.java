539dd8df6bd95841ecd26cd9255ed26d9f38a588
pari <parivashcheraghi40@gmail.com>
2025-08-06 12:14:24 +0330

    book repository updated

--- demo/src/main/java/com/example/demo/repository/BookRepository.java
+++ demo/src/main/java/com/example/demo/repository/BookRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
